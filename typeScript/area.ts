be50c0bd69abe96051cdce1589702eced5827f22
AlejandroAsensio <alejandro.asensio.alvarez@gmail.com>
2022-01-24 12:38:28 +0100

    typescript

--- typeScript/area.ts
+++ typeScript/area.ts
@@ -1,6 +1,6 @@
 function area(shape: string, width: number, height: number){
-    var area = width*height;
-    return "Soy un "+shape+" con un area de "+area+" cm cuadrados";
+    var total = width*height;
+    return "Soy un "+shape+" con un area de "+total+" cm cuadrados";
 }
 
-document.body.innerHTML = area("cuadrado",30,30);
+document.body.innerHTML = area("rectangulo",30,30);
