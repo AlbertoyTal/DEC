15dff6c512bc2636595fdaced45ad046cd0267b5
AlejandroAsensio <alejandro.asensio.alvarez@gmail.com>
2022-02-09 12:37:41 +0100

    angular ngstyle con ternario

--- angular/prueba/src/app/zapatillas/zapatillas.component.ts
+++ angular/prueba/src/app/zapatillas/zapatillas.component.ts
@@ -9,16 +9,31 @@ import { Zapatilla } from '../models/zapatillas';
 export class ZapatillasComponent implements OnInit {
   public titulo: string="Componente de Zapatillas";
   public zapatillas: Array<Zapatilla>;
+  public marcas: Array<String>;
 
   constructor() {
+    this.marcas = new Array;
     this.zapatillas = [
       new Zapatilla("Reebok Classic", "Reebok", "Verde", 123, true),
       new Zapatilla("Nike Runner", "Nike", "Negro", 113, true),
-      new Zapatilla("Adidas Air", "Adidas", "Blanco", 132, true)
-    ]
+      new Zapatilla("Nike Air", "Nike", "Rojo", 150, false),
+      new Zapatilla("Adidas Air", "Adidas", "Blanco", 132, true),
+      new Zapatilla("Adidas Tango", "Adidas", "Azul", 112, false),
+      new Zapatilla("Adidas Campeona", "Adidas", "Blanco", 135, true)
+    ];
+    
    }
 
   ngOnInit(): void {
+    this.getMarcas();
+  }
+
+  getMarcas(): void{
+    this.zapatillas.forEach((zapatilla, index)=>{
+      if(this.marcas.indexOf(zapatilla.marca) < 0){
+        this.marcas.push(zapatilla.marca);
+      }
+    });
   }
 
 }
